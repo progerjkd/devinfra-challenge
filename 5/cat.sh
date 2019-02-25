844524cb8bc7494f90a1603154bef949b645840f
Roger Vasconcelos <progerjkd@gmail.com>
2019-02-25 18:39:24 -0300

    ..

--- 5/cat.sh
+++ 5/cat.sh
@@ -3,11 +3,17 @@
 file=b.txt
 
 line_number=''
-
+blank=0
+num_line=1
 while IFS= read -r line
 do
-  echo "${line}"
+  echo "$line" | grep '^[[:space:]]*$' >/dev/null && blank=$(echo $blank + 1| bc) || blank=0
+  if [ $blank -le 1 ]; then
+    echo "$num_line $line\$" # 6 casas
+    num_line=$(echo $num_line + 1 | bc)
+  fi
 done <"$file"
 
 # se tiver conteúdo
-echo "${line}"
+#echo "$line"
+echo "$num_line $line"
