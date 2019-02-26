0bebad5d4dcc1b4974e469c9eb107c28e99f68af
Roger Vasconcelos <progerjkd@gmail.com>
2019-02-26 13:17:16 -0300

    Question completed.

--- 5/cat.sh
+++ 5/cat.sh
@@ -1,44 +1,55 @@
 #!/usr/bin/env bash
 
 
-
 show_help(){
-  echo "Usage: ${0##*/} [-sEn] [FILE]"
-  echo "Read FILE and write its contents to STDOUT. With no FILE or"
-  echo "when FILE is -, read from STDIN."
+  echo "Usage: ${0##*/} [OPTION]... [FILE]..."
+  echo "Concatenate FILE(s) to standard output."
+  echo
+  echo "With no FILE read standard input."
   echo
   echo -e "\t -s \t suppress repeated empty output lines"
   echo -e "\t -E \t display \$ at end of each line"
   echo -e "\t -n \t number all output lines"
   echo -e "\t -h \t display this help and exit"
-  echo -e "\t -v \t verbose mode"
-  exit 1
 }
 
 print_text(){
 
+  line="$1"
+  CRLF="$2"
+
   # -E flag
-  if [ $show_ends -eq 1 ]; then
+  if [ $show_ends -eq 1 ] && [ $CRLF -eq 1 ]; then
     line="${line}\$"
   fi
 
   # -n flag
   if [ $show_numbers -eq 1 ]; then
-    printf "%6s\t%s\n" "$num_line" "$line"
-    num_line=$((num_line + 1))
+    if [ $CRLF -eq 1 ]; then
+      printf "%6s\t%s\n" "$num_line" "$line"
+      num_line=$((num_line + 1))
+    else
+      printf "%6s\t%s" "$num_line" "$line"
+      num_line=$((num_line + 1))
+    fi
   else
-    printf "%s\n" "$line"
+    if [ $CRLF -eq 1 ]; then
+      printf "%s\n" "$line"
+    else
+      printf "%s" "$line"
+    fi
   fi
 }
 
-# Initializing variables
-input_file=""
-verbose=0
 
+# Initializing variables
+input_file=
+input_array=()
+msg_array=()
 suppress=0
 show_ends=0
 show_numbers=0
-
+exit_code=0
 OPTIND=1
 
 while getopts sEnh opt; do
@@ -52,59 +63,75 @@ while getopts sEnh opt; do
     n)
       show_numbers=1
       ;;
-    v)
-      verbose=$((verbose + 1))
-      ;;
     h)
       show_help >&2
+      exit 0
+      ;;
+    *)
+      exit 1
       ;;
   esac
 done
 
-shift "$((OPTIND-1))"   # Discard the options and sentinel --
+# Discard the options and sentinel --
+shift "$((OPTIND-1))"
 
-if [ $# = 1 ]; then
-  input_file="$1"
-  if [ ! -r $input_file ]; then
-    echo "Error: file $input_file not found or is not readable"
-    exit 1
-  fi
+if [ $# = 0 ]; then
+  unset input_file
+  input_array+=('')
 else
-  show_help >&2
+  for file in "$@"
+  do
+    if [ ! -r "$file" ]; then
+      #echo "${0##*/}: '$file': file not found or not readable"
+      msg_array+=("${0##*/}: $file: No such file or directory")
+      exit_code=1
+    else
+      input_array+=($file)
+    fi
+  done
 fi
+
 
 blank=0
 num_line=1
 
-while IFS= read -r line
-do
-  # if line is blank, increment $blank until a line differ than blank
-  echo "$line" | grep '^[[:space:]]*$' >/dev/null && blank=$(echo $blank + 1| bc) || blank=0
+# iterate over the input files or STDIN
+for input_file in "${input_array[@]}"; do
+  while IFS= read -r line
+  do
+    # if line is blank, increment $blank until a line differ than blank
+    echo "$line" | grep -q '^[[:space:]]*$' && blank=$((blank + 1)) || blank=0
 
-  if [ $suppress -eq 1 ] && [ $blank -le 1 ]; then
+    if [ $suppress -eq 1 ] && [ $blank -le 1 ]; then
 
-    print_text
+      print_text "$line" "1"
 
-  elif [ $suppress -eq 0 ]; then
+    elif [ $suppress -eq 0 ]; then
 
-    print_text
+      print_text "$line" "1"
 
+    fi
+  done <"${input_file:-/dev/stdin}"
+
+
+  # print the last line if it does not end with CRLF
+  blank=0
+  if [ "$line" != "" ]; then
+    if [ $suppress -eq 1 ] && [ $blank -le 1 ]; then
+
+      print_text "$line" "0"
+
+    elif [ $suppress -eq 0 ]; then
+
+      print_text "$line" "0"
+
+    fi
   fi
-done <"$input_file"
+done
 
-# se tiver conteúdo
-echo "DEBUG: $line"
-if [ "$line" != "" ]; then
-  echo "dentro"
-  if [ $suppress -eq 1 ] && [ $blank -le 1 ]; then
-echo 1
-    print_text
-echo 2
-  elif [ $suppress -eq 0 ]; then
-echo 3
-    print_text
-echo 4
-  fi
-fi
-#echo "$line"
-#echo "$num_line $line"
+# error messages, if any
+for msg in "${msg_array[@]}"; do
+  echo $msg
+done
+exit $exit_code
