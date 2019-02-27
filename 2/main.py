b94d13828980f013317e750e28cf3c9e6c23dc82
Roger Vasconcelos <progerjkd@gmail.com>
2019-02-27 19:29:57 -0300

    q2

--- 2/main.py
+++ 2/main.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 import re
-from collections import Counter
 import sys
-#2019-02-26 21:56:28 - conn: 200.129.20.33, proc: 20, err: 404, val: /index.html
+
 
 def log_reader(logfile):
-    myregex = r'\d{1,4}-\d{1,2}\-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}'
+    pattern1 = r'err: \d+,'
+    pattern2 = r'\d+'
 
     with open(logfile) as f:
-        log = f.read()
-        my_datelist = re.findall(myregex,log)
-        datecount = Counter(my_datelist)
-        for key in sorted(datecount.iterkeys()):
-            print("Date " + "=> " + str(key) + " " + "Count "  + "=> " + str(datecount[key]))
+        log = f.readlines()
+        last_line = log[-1]
+        field = re.search(pattern1, last_line).group()
+        error_rate = int(re.search(pattern2, field).group())
+
+        if error_rate <= 10:
+            print(f"OK - Error rate: {error_rate} err/s.")
+            sys.exit(0)
+        elif 10 < error_rate <= 20:
+            print(f"WARNING - Error rate: {error_rate} err/s.")
+            sys.exit(1)
+        elif error_rate > 20:
+            print(f"CRITICAL - Error rate: {error_rate} err/s.")
+            sys.exit(2)
+        else:
+            print(f"UNKNOWN - Error rate: {error_rate} err/s.")
+            sys.exit(3)
+
 
 if __name__ == '__main__':
     log_reader("error.log")
